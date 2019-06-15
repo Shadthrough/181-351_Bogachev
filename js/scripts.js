e2e2abdb3d6a2337f1e8729b7eee746cd80fea65
Shadthrough <maxim.b1229@gmail.com>
2019-06-15 15:29:43 +0300

    working on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var toggle_traf = document.querySelector("#b1");
 toggle_traf.addEventListener("click", show_traf);
+var cur_tars = {
+    firsttar : 0
+};
+var summ = 0;
 
 function show_traf() {
     var tars = document.querySelector(".tars");
@@ -7,4 +11,20 @@ function show_traf() {
     tars.style.opacity = "1";
     var cur_tar = document.querySelector("#ctar");
     cur_tar.style.opacity = "0";
+}
+
+function tarchange(event) {
+    var targ = event.target;
+    var price = targ.querySelector(".pr");
+    recout();
+}
+
+function recount() {
+    
+}
+
+var all_tars = document.querySelectorAll(".tars .box");
+
+for (let i = 0; i < all_tars.length; i++) {
+    all_tars[i].addEventListener("click", tarchange);   
 }
